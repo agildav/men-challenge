623b2cdc14264724c0a35fd8cc8ce010dfc17516
Alberto A. Gil D <agildav@gmail.com>
2020-04-08 20:41:37 -0400

    Add post's author id fix on creation (#2)

--- src/middlewares/post/createPost.js
+++ src/middlewares/post/createPost.js
@@ -2,8 +2,11 @@ import postService from '../../services/postService';
 
 const createPost = async (req, res, next) => {
   try {
-    const user = await postService.create(req.body);
-    return res.status(200).send(user);
+    const postBody = { ...req.body, author: req.user._id };
+
+    const post = await postService.create(postBody);
+
+    return res.status(200).send(post);
   } catch (err) {
     return next(err);
   }
