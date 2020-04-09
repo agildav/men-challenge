b37ba5e72064478eb9da7dac9cac3595418ec410
agildav <agildav@gmail.com>
2020-04-09 17:50:50 -0400

    Add create comment and get comments functionality

--- src/models/post.js
+++ src/models/post.js
@@ -7,6 +7,7 @@ export const MAX_BODY_LENGTH = 200;
 
 export const TITLE_FIELD_NAME = 'title';
 export const KEYWORDS_FIELD_NAME = 'keywords';
+export const COMMENTS_FIELD_NAME = 'comments';
 export const AUTHOR_FIELD_NAME = 'author';
 export const BODY_FIELD_NAME = 'body';
 
@@ -29,6 +30,13 @@ const PostSchema = new Schema({
     required: true,
     maxlength: MAX_BODY_LENGTH,
   },
+  comments: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Comment',
+      required: false,
+    },
+  ],
   date: { type: Date, default: Date.now },
 });
 
--- src/services/postService.js
+++ src/services/postService.js
@@ -1,13 +1,29 @@
 import models from '../models';
 
-const { Post } = models;
+const { Post, Comment } = models;
 
 const create = (post) => Post.create(post);
 
 const findAll = (parameters) => Post.find({ ...parameters });
 
-const findById = (id) => Post.findById(id);
+const findById = (id) => Post.findById(id).populate([{ path: 'comments' }]);
 
-const postService = { create, findAll, findById };
+const createComment = (comment) => Comment.create(comment);
+
+// eslint-disable-next-line arrow-body-style
+const addCommentToPost = (comment, postId) => {
+  return Post.findOneAndUpdate(
+    { _id: postId },
+    { $push: { comments: comment } },
+  );
+};
+
+const postService = {
+  create,
+  findAll,
+  findById,
+  createComment,
+  addCommentToPost,
+};
 
 export default postService;
